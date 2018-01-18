371a7ccfda689b2fc1f0d1576c61be735fb0eb83
kjschelling <kjschelling@gmail.com>
2018-01-18 16:34:04 -0500

    Add editable to ingredient_serializer
    - used "def editable
       scope == object.user
      end "

--- app/serializers/info_serializer.rb
+++ app/serializers/info_serializer.rb
@@ -1,9 +1,11 @@
 class InfoSerializer < ActiveModel::Serializer
   attributes :id, :additional_info, :ingredient
   # belongs_to :ingredient
+
   def ingredient
     object.ingredient.id
   end
+
 end
 
 # def ingredient
--- app/serializers/ingredient_serializer.rb
+++ app/serializers/ingredient_serializer.rb
@@ -1,8 +1,13 @@
 class IngredientSerializer < ActiveModel::Serializer
-  attributes :id, :name, :note, :user, :infos
+  attributes :id, :name, :note, :user, :infos, :editable
   belongs_to :user
+
+  def editable
+   scope == object.user
+  end
 
   def infos
     object.infos.pluck(:id)
   end
+
 end
